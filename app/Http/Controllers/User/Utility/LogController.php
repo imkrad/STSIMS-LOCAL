8f3dd3d61cf04516c2749b7e9b9f56e836ebb6b8
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-05-28 09:07:33 +0800

    sdada

--- app/Http/Controllers/User/Utility/LogController.php
+++ app/Http/Controllers/User/Utility/LogController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers\User\Utility;
 
+use App\Models\Log;
 use Spatie\Activitylog\Models\Activity;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\ActivityResource;
+use App\Http\Resources\AuthenticationResource;
 
 class LogController extends Controller
 {
     public function activity(Request $request){
         $data = Activity::with('causer.profile')->orderBy('created_at','DESC')->paginate($request->count);
         return ActivityResource::collection($data);
+    }
+
+    public function authentication(Request $request){
+        $data = Log::lists($request->counts);
+        return AuthenticationResource::collection($data);
     }
 }
